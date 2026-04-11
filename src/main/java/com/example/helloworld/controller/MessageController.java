ae049f0d100ae94ca1ea1d7e1e17a3077cca5780
madjid <madjid04@gmail.com>
2026-04-11 16:46:26 +0200

    Add JSON logging for ELK Stack

--- src/main/java/com/example/helloworld/controller/MessageController.java
+++ src/main/java/com/example/helloworld/controller/MessageController.java
@@ -2,15 +2,15 @@ package com.example.helloworld.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.helloworld.HelloworldApplication;
 import com.example.helloworld.entity.Message;
 import com.example.helloworld.repository.MessageRepository;
 
@@ -18,8 +18,7 @@ import com.example.helloworld.repository.MessageRepository;
 @RequestMapping("/hello")
 public class MessageController {
 
-	@Value("${spring.datasource.url:NON DEFINI}")
-    private String datasourceUrl;
+	private static final Logger log = LoggerFactory.getLogger(HelloworldApplication.class);
 
 	private final MessageRepository repository;
 
@@ -27,20 +26,18 @@ public class MessageController {
         this.repository = repository;
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void onReady() {
-        System.out.println(">>> DATASOURCE URL: " + datasourceUrl);
-    }
-
     // GET — lire tous les messages
     @GetMapping
     public List<Message> getAll() {
+    	log.info("GET /hello — {} messages trouvés", repository.count());
         return repository.findAll();
     }
 
     // POST — créer un message
     @PostMapping
     public Message create(@RequestBody Message message) {
-        return repository.save(message);
+    	Message saved = repository.save(message);
+    	log.info("POST /hello — message créé id={} content={}", saved.getId(), saved.getContent());
+        return saved;
     }
 }
